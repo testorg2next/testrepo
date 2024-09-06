341d91f9b94e8af75a19b3556f3c29710df3e54e
Daniel Pressler <Daniel.Pressler@govsco.com>
2024-09-06 12:17:37 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,24 @@ from setuptools import setup
 setup(
     name='githubtestorg2-testrepo-testpkg',
     version='0.0.3',
+    description="A sample Python project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/githubtestorg2/testrepo",
     install_requires=[
         'numpy>=1.11.0',
         'scipy',
     ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    license='MIT',
 )
